30da285b292c92bd12ba9214ce156b0ea38a957c
stepann0 <45241991+stepann0@users.noreply.github.com>
2022-08-05 20:41:23 +0400

    Add equation solver

---
+++ algorithms/solve_equation.py
@@ -0,0 +1,49 @@
+import re
+
+class Solution:
+    """Simple linear equations solver."""
+
+    # Convert term to int
+    def to_int(self, s: str) -> int:
+        s = s.replace("x", "")
+        if s == "-": # s was '-x'
+            return -1
+        if s == "+" or s == '': # s was '+x' or 'x'
+            return 1
+        return int(s)
+
+    # Calculate all xs and coefficients 
+    def parse(self, expr):
+        x = 0
+        coef = 0
+        regexp = r"[-+]?\d*x?"
+        matches = re.finditer(regexp, expr)
+        for m in matches:
+            s = m.group(0)
+            # Escape empty matches
+            if len(s) == 0:
+                continue
+            if "x" in s:
+                x += self.to_int(s)
+            else:
+                coef += self.to_int(s)
+        return x, coef
+
+    # Solve equation
+    def solveEquation(self, equation: str) -> str:
+        left, right = equation.split("=")
+
+        left_x, left_coef = self.parse(left)
+        right_x, right_coef = self.parse(right)
+
+        total_x = left_x - right_x
+        total_coef = right_coef - left_coef
+
+        if total_coef == 0 and total_x == 0:
+            return "Infinite solutions"
+        if total_x == 0 :
+            return "No solution"
+        return f"x={total_coef//total_x}"
+
+
+print(Solution().solveEquation("x+5-3+x=6+x-2"))
