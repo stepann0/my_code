478f195daf22ee8c4747b22b7736bf1a91067ff9
stepann0 <45241991+stepann0@users.noreply.github.com>
2022-08-09 16:00:08 +0400

    Add TwoSumII

--- algorithms/two_sum.go
+++ algorithms/two_sum.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func TwoSumWithoutDict(nums []int, target int) []int {
 	for i, n := range nums {
 		m := target - n
 		for j := 0; j < i; j++ {
 			if nums[j] == m {
+				fmt.Printf("%d + %d = %d\n", nums[i], nums[j], target)
 				return []int{i, j}
 			}
 		}
@@ -28,6 +32,25 @@ func TwoSum(nums []int, target int) []int {
 	return []int{}
 }
 
+func TwoSumII(nums []int, target int) []int {
+	sort.Ints(nums)
+	for l, r := 0, len(nums)-1; l < r; {
+		sum := nums[l] + nums[r]
+		if sum > target {
+			r--
+		} else if sum < target {
+			l++
+		} else {
+			fmt.Printf("%d + %d = %d\n", nums[l], nums[r], target)
+			return []int{nums[l], nums[r]}
+		}
+	}
+	return []int{}
+}
+
 func main() {
-	fmt.Println(TwoSum([]int{3, 5, 1, 0, 7}, 100))
+	nums, target := []int{5, 6, 1, 3, 7}, 13
+	fmt.Printf("%v\n\n", TwoSumII(nums, target))
+	fmt.Printf("%v\n\n", TwoSum(nums, target))
+	fmt.Printf("%v\n", TwoSumWithoutDict(nums, target))
 }
